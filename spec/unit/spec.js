c754e9d9e88bff50d665b8ac6011ec4a15e6f1e2
Richard Klancer <rklancer@dhcp199.concord.org>
2010-03-18 15:56:05 -0400

    Refactor of testcase

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,19 +1,24 @@
 
 describe 'Comments'
   describe '.showingAndHiding()'
+    before_each
+      var $comment_sample = $(fixture('comment-sample'));
+      var expandables = $comment_sample.find('.expandable');
+    end
+    
     it 'should show a hidden body'
-		$(fixture('comment-sample')).find('.expandable').each(function (){
+		expandables.each(function () {
 			$(this).find('.body').hide();
 			$(this).find('.body').should.be_hidden();
-			toogleBody($(this).find('.title'));
+			toggleBody($(this).find('.title'));
 			$(this).find('.body').should.be_visible();
 		});
     end
 	it 'should hide a visible body'
-		$(fixture('sample-comments')).find('.expandable').each(function (){
+		expandables.each(function () {
 			$(this).find('.body').show();
 			$(this).find('.body').should.be_visible();
-			toogleBody($(this).find('.title'));
+			toggleBody($(this).find('.title'));
 			$(this).find('.body').should.be_hidden();
 		});
     end
