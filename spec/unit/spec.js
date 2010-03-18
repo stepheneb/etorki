39f1f7f1e10c051ab1b6e609ba91a4067a64a7c3
sfentress <sfentress@concord.org>
2010-03-18 16:50:32 -0400

    adding elements to dom

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -2,8 +2,13 @@
 describe 'Comments'
   describe '.showingAndHiding()'
     before_each
-      $comment_sample = $(fixture('comment-sample'));
-      $expandables = $comment_sample.find('.expandable');
+	$('body').append("<div id='test'>");
+		$('#test').append(fixture('comment-sample'));
+      $expandables = $('.expandable');
+    end
+
+	after_each
+		$('#test').html('');
     end
     
     it 'should show a hidden body'
@@ -12,13 +17,10 @@ describe 'Comments'
 			$(this).find('.body').should.be_hidden();
 			toggleBody($(this).find('.title'));
 			$(this).find('.body').should.be_visible();
-			toggleBody($(this).find('.title'));
-			$(this).find('.body').should.be_hidden();
 		});
     end
 	it 'should hide a visible body'
 		$expandables.each(function () {
-			$(this).find('.body').hide();
 			$(this).find('.body').show();
 			$(this).find('.body').should.be_visible();
 			toggleBody($(this).find('.title'));
