d5a427f8a7eddcf0676600c2bb5a0705b1373a6e
Noah Paessel <knowuh@gmail.com>
2010-03-19 09:59:56 -0400

    added gisted junit xml formatter.

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,10 +1,13 @@
+
 load('./spec/support/env.rhino.js')
 load('./spec/support/jquery.js')
 load('./spec/lib/jspec.js')
 load('./spec/lib/jspec.xhr.js')
 load('./spec/lib/jspec.jquery.js')
+load('./spec/lib/junit.xml.js')
 load('lib/etorki.js')
 load('spec/unit/spec.helper.js')
+
 
 JSpec
 .exec('spec/unit/spec.js')
