71e8ec23115ad813f8971a11eea3601891e81108
Richard Klancer <rklancer@dhcp199.concord.org>
2010-03-19 11:51:03 -0400

    Factored rhino reporters into rhino-terminal.js and rhino.js for jUnit
    Also added spec/jspec-rhino script to run tests

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,18 +1,4 @@
-
-load('./spec/support/env.rhino.js')
-load('./spec/support/jquery.js')
-load('./spec/lib/jspec.js')
-load('./spec/lib/jspec.xhr.js')
-load('./spec/lib/jspec.jquery.js')
-load('./spec/support/junit.xml.js')
-load('lib/etorki.js')
-load('spec/unit/spec.helper.js')
-
-
-JSpec
-.exec('spec/unit/spec.js')
-.exec('unit/jquery_spec.js')
-// .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
-// .report()
+load('./spec/rhino-common.js')
+specResults
 .run({ reporter: JSpec.reporters.JUnit, fixturePath: 'spec/fixtures' } )
 .report()
