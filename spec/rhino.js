45931c9f21dde93e2ea67f6f3642018d17463ca0
Richard Klancer <rklancer@rklancer-unibody.local>
2010-03-18 21:12:51 -0400

    set rhino.js to what we want given a working jspec+Env.js invocation
    spec/rhino.js executes the tests we want (though they fail for unknown reasons) if you invoke jspec as follows:
    
    java -jar spec/support/js.jar -opt -1 spec/rhino.js

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,14 +1,12 @@
-
 load('./spec/support/env.rhino.js')
-Envjs("./comments.html");
 load('./spec/support/jquery.js')
 load('./spec/lib/jspec.js')
 load('./spec/lib/jspec.xhr.js')
 load('./spec/lib/jspec.jquery.js')
-// load('lib/etorki.js')
+load('lib/etorki.js')
 // load('spec/unit/spec.helper.js')
 
 JSpec
-.exec('spec/unit/true_spec.js')
+.exec('spec/unit/spec.js')
 .run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
 .report()
