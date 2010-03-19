2ff873e2692d01830c31710c1bc00e9bf17dcb58
Noah Paessel <knowuh@gmail.com>
2010-03-19 10:05:46 -0400

    Moved the junit xml formatter to support

--- spec/rhino.js
+++ spec/rhino.js
@@ -4,12 +4,13 @@ load('./spec/support/jquery.js')
 load('./spec/lib/jspec.js')
 load('./spec/lib/jspec.xhr.js')
 load('./spec/lib/jspec.jquery.js')
-load('./spec/lib/junit.xml.js')
+load('./spec/support/junit.xml.js')
 load('lib/etorki.js')
 load('spec/unit/spec.helper.js')
 
 
 JSpec
 .exec('spec/unit/spec.js')
-.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+.run({ formatter : JSpec.formatters.JUnit, fixturePath: 'spec/fixtures' } )
+//.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
 .report()
